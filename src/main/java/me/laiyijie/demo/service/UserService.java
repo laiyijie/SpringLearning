2d1e3face259907374561cf653d970e642b7bbf8
laiyijie <449132062@qq.com>
2016-12-04 09:16:30 +0800

    09 Spring 事务管理（@Transactional）基本使用

--- src/main/java/me/laiyijie/demo/service/UserService.java
+++ src/main/java/me/laiyijie/demo/service/UserService.java
@@ -2,6 +2,4 @@ package me.laiyijie.demo.service;
 
 public interface UserService {
 	void createAccount(String username ,String password,String name) throws Exception;
-	void createAcc(String username ,String password ,String name ) throws Exception;
-	public void createAccou(String username, String password, String name) throws Exception;
 }
--- src/main/java/me/laiyijie/demo/service/UserServiceImpl.java
+++ src/main/java/me/laiyijie/demo/service/UserServiceImpl.java
@@ -1,95 +1,34 @@
 package me.laiyijie.demo.service;
 
 import java.sql.Connection;
-import java.sql.Statement;
 
 import org.apache.commons.dbcp.BasicDataSource;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DataSourceUtils;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import me.laiyijie.demo.dao.AccountMapper;
-import me.laiyijie.demo.dao.PocketMapper;
-import me.laiyijie.demo.domain.Account;
-import me.laiyijie.demo.domain.Pocket;
-
 
 @Service
 public class UserServiceImpl implements UserService {
 
 	@Autowired
 	private BasicDataSource dataSource;
-	@Autowired
-	private PocketMapper pocketMapper;
-	@Autowired
-	private AccountMapper accountMapper;
-	@Transactional
+
+	@Transactional(rollbackFor={Exception.class})
 	public void createAccount(String username, String password, String name) throws Exception {
 
 		Connection connection = DataSourceUtils.getConnection(dataSource);
 
-		String insertAccountSql = "INSERT INTO account (username, password, name,create_time) VALUES('" + username
-				+ "','" + password + "','" + name + "','" + System.currentTimeMillis() + "')";
-		String insertPocketSql = "INSERT INTO pocket (username,current_money) VALUES('" + username + "','" + 0 + "')";
-		Statement statement = connection.createStatement();
-		statement.execute(insertAccountSql);
-		
-		System.out.println("afther account sql");
+		connection.createStatement().execute("INSERT INTO account (username, password, name,create_time) VALUES('"
+				+ username + "','" + password + "','" + name + "','" + System.currentTimeMillis() + "')");
+
 		if (username == "lailai") {
 			throw new RuntimeException("lailai");
 		}
 
-		statement.execute(insertPocketSql);
-		
-		System.out.println("afther POCKET sql");
-	}
-	
-	@Transactional
-	public void createAcc(String username, String password, String name) throws Exception {
-		Account account = new Account();
-		
-		account.setCreate_time(System.currentTimeMillis());
-		account.setName(name);
-		account.setPassword(password);
-		account.setUsername(username);
-
-		accountMapper.insert(account);
-		
-		System.out.println("afther account insert");
-		
-		if (username == "lailai") {
-			throw new RuntimeException("lailai");
-		}
-		Pocket pocket = new Pocket();
-		pocket.setCurrent_money(0);
-		pocket.setUsername(username);
-		
-		pocketMapper.insert(pocket);
+		connection.createStatement()
+				.execute("INSERT INTO pocket (username,current_money) VALUES('" + username + "','" + 0 + "')");
 
 	}
-	
-	
-	@Transactional
-	public void createAccou(String username, String password, String name) throws Exception {
-		
-		JdbcTemplate jTemplate = new JdbcTemplate(dataSource);
 
-		String insertAccountSql = "INSERT INTO account (username, password, name,create_time) VALUES('" + username
-				+ "','" + password + "','" + name + "','" + System.currentTimeMillis() + "')";
-		String insertPocketSql = "INSERT INTO pocket (username,current_money) VALUES('" + username + "','" + 0 + "')";
-		
-		jTemplate.execute(insertAccountSql);
-		
-		System.out.println("afther account sql");
-		if (username == "lailai") {
-			throw new RuntimeException("lailai");
-		}
-
-		jTemplate.execute(insertPocketSql);
-		
-		System.out.println("afther POCKET sql");
-		
-	}
 }
